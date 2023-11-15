b7a7fba68e76a6f2729d88da07691235a9795275
Ajeem shaik <132434412+ajeemshaik7@users.noreply.github.com>
2023-11-15 11:15:18 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.jpg'
+
+
+const Navbar = () => {
+
+const [menu,setMenu] = useState("shop");
+
+
+  return (
+    <div className='navbar'> 
+     <div className='nav-logo'>
+         <img src={logo} alt="" />
+         <p>BISMILLAH IRON HARDWARE & GENERAL STORES</p>
+     </div>
+     <ul className='nav-menu'>
+      <li onClick={()=>{setMenu("products")}}>PRODUCTS{menu==="products"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("contacts")}}>CONTACTS{menu==="contacts"?<hr/>:<></>}</li>
+     </ul>
+    </div>
+  )
+}
+
+export default Navbar
